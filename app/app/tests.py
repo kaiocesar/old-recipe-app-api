59bef934843c37d4eb0dcc168846800089559cd0
Kaio Cesar <tecnico.kaio@gmail.com>
2020-05-23 09:48:02 -0300

    red tdd

---
+++ app/app/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+class CalcTest(TestCase):
+
+    def test_add_two_numbers(self):
+        self.assertEquals(add(4, 19), 23)
+
+    def test_subtract_two_positive_numbers(self):
+        self.assertEquals(subtract(30, 50), -20)
+
+    def test_subtract_two_negative_numbers(self):
+        self.assertEquals(subtract(-1, -10), -11)
