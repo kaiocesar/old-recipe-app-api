e09f5620ce514354d87bd43277018e783a5e547f
Kaio Cesar <tecnico.kaio@gmail.com>
2020-06-06 21:52:24 -0300

    add user_exists and password_too_short

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # recipe-app-api
-Recipe App Apii
+Recipe App Api
 
+
+### run the unit tests
+`docker-compose run sh -c "python manage.py test && flake8`
 
 
 [![Build Status](https://travis-ci.com/kaiocesar/recipe-app-api.svg?branch=master)](https://travis-ci.com/kaiocesar/recipe-app-api)
--- app/core/tests/test_user_api.py
+++ app/core/tests/test_user_api.py
@@ -34,7 +34,20 @@ class PublicUserApiTests(TestCase):
         self.assertNotIn('password', res.data)
 
     def test_user_exists(self):
-        pass
+        payload = {'email': 'kaio@cesar.com',
+                   'password': 'secretpassword'}
+        create_user(**payload)
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short(self):
-        pass
+        payload = {'email': 'kaio@cesar.com',
+                   'password': 'secretpassword'}
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+        self.assertFalse(user_exists)
